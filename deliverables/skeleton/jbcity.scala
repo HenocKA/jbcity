188d289e603b20f63dabbb2d76ef111c3618a9fc
Souhire KENAWI <souhire@souhire.(none)>
2013-11-07 16:11:12 +0100

    Skeleton

---
+++ deliverables/skeleton/jbcity.scala
@@ -0,0 +1,101 @@
+//Déclaration des différentes classes, il manque à rajouter les attributs, les méthodes...
+
+abstract class Map {
+  def map(i:Int, j:Int):Array[String]
+}
+
+abstract class Maire {
+  
+}
+
+abstract class Habitant{
+
+}
+
+abstract class Ville{
+
+}
+
+abstract class Random_Event{
+
+}
+
+abstract class Epidemie extends Random_Event{
+
+}
+
+abstract class Catastrophe extends Random_Event{
+
+}
+
+abstract class Attaque extends Random_Event{
+
+}
+
+abstract class Reseau{
+
+}
+
+abstract class Reseau_elec extends Reseau{
+
+}
+
+abstract class Reseau_rout extends Reseau{
+
+}
+
+abstract class Reseau_ferr extends Reseau{
+
+}
+
+abstract class Trancon_elec {
+
+}
+
+abstract class Trancon_rout {
+
+}
+
+abstract class Trancon_ferr{
+
+}
+
+abstract class Infrastructure{
+
+}
+
+abstract class Transport extends Infrastructure{
+
+}
+
+abstract class Securite extends Infrastructure{
+
+}
+
+abstract class Industrie extends Infrastructure{
+
+}
+
+abstract class Commerce extends Infrastructure{
+
+}
+
+abstract class Oeuvre extends Infrastructure{
+
+}
+
+abstract class Habitation extends Infrastructure{
+
+}
+
+abstract class Loisir extends Infrastructure{
+
+}
+
+abstract class Impot {
+
+}
+
+abstract class Season_Event{
+
+}
