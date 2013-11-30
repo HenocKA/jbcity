e2e486ced1b5ddbb33bb45d2bce276754a4c835f
mnabe <mohamedsalounabe@yahoo.fr>
2013-11-30 15:20:37 +0100

    Ajout de la classe Coordinates et modification des noms des classes

--- deliverables/skeleton/src/com/jbcity/sim/Event/random_Event.scala
+++ deliverables/skeleton/src/com/jbcity/sim/Event/random_Event.scala
@@ -1,5 +1,25 @@
 package com.jbcity.Event
 
 class random_Event {
+	//Déclaration des variables
+	var typeEvent:String;
+	var dead:Int = 0;
+	var wounded:Int = 0;
+	var missing:Int = 0;
+	var level:Int = 0;
+  	//Déclaration des Getters
+	abstract def getLevel:Int ;
+	abstract def getTime:Int ;
+	abstract def getDead:Int ;
+	abstract def getWounded:Int ;
+	abstract def getType:String ;
+	abstract def getMissing:Int ;
+	//Déclaration des Setters
+	abstract def setLevel = (level:Int) {this.level = level} 
+	abstract def setTime = (time:Int) {this.time = time}
+	abstract def setDead = (dead:Int) {this.dead = dead}
+	abstract def setWounded = (wounded:Int) {this.wounded = wounded}
+	abstract def setType = (typeEvent:String) {this.typeEvent = typeEvent}
+	abstract def setMissing = (missing:Int) {this.missing = missing}
 	
 }
---
+++ deliverables/skeleton/src/com/jbcity/sim/coordinates.scala
@@ -0,0 +1,19 @@
+package com.jbcity.entity
+
+class coordinates (x1 : Int, y1 : Int, x2 : Int, y2 : Int)  {
+	def getX1 : Int =  {
+	  return this.x1;
+	}
+	
+	def getY1 : Int =  {
+	  return this.y1;
+	}
+	
+	def getX2 : Int =  {
+	  return this.x2;
+	}
+	
+	def getY2 : Int =  {
+	  return this.y2;
+	}
+}
