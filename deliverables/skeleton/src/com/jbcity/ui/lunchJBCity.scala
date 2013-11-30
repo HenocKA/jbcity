607bbae6f11255a860312e10c1c387e244483c5a
ndjomo <louis.bertin2013@gmail.com>
2013-11-30 15:40:53 +0100

    redimensionnement fenêtre de la classe de lancement du jeux

--- deliverables/skeleton/src/com/jbcity/ui/lunchJBCity.scala
+++ deliverables/skeleton/src/com/jbcity/ui/lunchJBCity.scala
@@ -6,31 +6,31 @@ import scala.swing.event.ButtonClicked
 object lunchJBCity extends SimpleSwingApplication {
   def top = new MainFrame() {
     title = "JBCity GAME"
-    size= new Dimension (1000,1000)
-    
-    val Load = new Button {
-      text = "Charger le jeux"
-    }
-    val label = new Label {
-      text = "Lancemenrt de JBCity..."
-    }	
-    
+    preferredSize = new Dimension(200,250) 
+      
+    val labVide = new Label {text = "    "}
+    val Load = new Button {text = "Charger le jeux"}
+    val label = new Label {text = "Lancemenrt de JBCity..."}	
     val labName = new Label{text = "Votre nom"}
-    val txtName = new TextField("")
-    
+    val txtName = new TextField{text=""; preferredSize = new Dimension(15,30) }
     val labCity = new Label{text = "Nom de votre ville"}
-    val txtNameCity = new TextField("")
-    
+    val txtNameCity = new TextField{text=""; preferredSize = new Dimension(15,30) }
     val closeBtn   = new Button(Action("Fermer") {this.dispose()})
 
     
     contents = new BoxPanel(Orientation.Vertical) {
-      contents += new FlowPanel{
-        labName
-        txtName
-      }
+
+      contents += labName
+      contents += txtName
+      
+      contents += labCity
+      contents += txtNameCity
+      
+      contents += labVide
       contents += Load
-      contents += label
+      
+      contents += labVide
+      contents += labVide
       contents += closeBtn
       border = Swing.EmptyBorder(30, 30, 10, 30)
     }
