8c979a7b384c051f4fa22989fecf5d57f6cd6f00
Henoc Khouilla <henoc.khouilla@gmail.com>
2014-01-06 05:24:19 +0100

    Console User Interface

---
+++ deliverables/version1.2/src/main/scala/ui/console/ConsoleDisplay.scala
@@ -0,0 +1,66 @@
+package main.scala.ui.ConsoleUI
+
+import main.scala.sim._
+import main.scala.sim.infrastructure._
+
+class ConsoleDisplay{
+  
+  def displayCity(city:City){	
+    val ord = city.ord
+    val abs = city.abs
+    val map = city.map
+		
+    for (x <- 0 to abs-1)
+      print("    "+x+" ")
+    println("")
+    for (i <- 0 to abs-1){
+      for(x <-0 to abs-2)
+        print(" ----- ")
+      println("")
+      print(i+" ")
+      for (j <- 0 to ord-1){
+    	print(" | ");
+        print(displayCell(map(i)(j)));
+    	//print("| ");
+      }
+      print(" |")
+      println("");
+    }
+  }	
+
+	
+  def displayCell(obj:JBObject): String = obj match  {
+	
+    case null => "\033[30memp\033[0m"
+      
+    case _:InfrastructureCommercial =>  "\033[31mcom\033[0m"
+			
+    case _:InfrastructureEntertainment =>  "\033[32mloi\033[0m"
+
+    case _:InfrastructureProduction =>  "\033[33mpro\033[0m"
+
+    case _:InfrastructureResidential =>  "\033[34mres\033[0m"
+    
+    case _:InfrastructureSecurity =>  "\033[35mloi\033[0m"
+    
+    case _:InfrastructureSocial =>  "\033[36msoc\033[0m"
+	
+    case _:InfrastructureTransport =>  "\033[37mtra\033[0m"
+      
+		
+  }
+  
+  def displayInfrastructureType(){
+    println("1: Commercial")
+    println("2: Entertainment")
+    println("3: Production")
+    println("4: Residential")
+    println("5: Security")
+    println("6: Social")
+    println("7: Transport")
+  }
+  
+  def displayIncorrectChoice(){
+    
+  }
+}
